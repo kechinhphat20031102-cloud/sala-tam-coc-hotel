edba7b5d6883fd7fdfc7304cb8cea889d5dcdb32
kechinhphat20031102-cloud <kechinhphat20031102@gmail.com>
2026-09-07 17:01:56 +0700

    Auto-sync room data & photo gallery from Admin Dashboard

--- assets/js/admin-data.js
+++ assets/js/admin-data.js
@@ -32,10 +32,10 @@ const DEFAULT_SALA_DATA = {
       "view": "City & Street View Window",
       "viewVi": "Cửa sổ hướng phố",
       "viewFr": "Fenêtre vue ville et rue",
-      "cover": "Ảnh Sala Tam Coc Hotel & Spa/Superior Double city view/ANT_4832.jpg",
+      "cover": "assets/images/rooms/superior-double_1788774879632_1.jpg",
       "photos": [
-        "Ảnh Sala Tam Coc Hotel & Spa/Superior Double city view/ANT_4832.jpg",
         "assets/images/rooms/superior-double_1788774879632_1.jpg",
+        "Ảnh Sala Tam Coc Hotel & Spa/Superior Double city view/ANT_4832.jpg",
         "Ảnh Sala Tam Coc Hotel & Spa/Superior Double city view/ANT_4807.jpg",
         "Ảnh Sala Tam Coc Hotel & Spa/Superior Double city view/ANT_4818.jpg",
         "Ảnh Sala Tam Coc Hotel & Spa/Superior Double city view/ANT_4825.jpg",
